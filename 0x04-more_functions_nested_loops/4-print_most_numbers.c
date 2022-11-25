ce4a149627bdd29056e53d24b6ab6af469ff8977
Sam Onyeji <onyejiteamwork@yahoo.com>
2022-11-25 03:51:02 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers- Entry
+ *Description: Print numbers expect from 2 and 4
+ *Return: Void
+*/
+
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
