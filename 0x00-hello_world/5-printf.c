789dbe13a72458daf0af9f5c05453edc9da7fc43
Sam Onyeji <onyejiteamwork@yahoo.com>
2022-11-25 03:13:33 -0800

    0-preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main-main-entry point for the program.
+ *
+ *Returns: returns zero if no erros, otherwise returns non-zero value.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcomeis a piece of art\n");
+	return (0);
+}
